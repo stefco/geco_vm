fa5cf1d743e12d509185ab54dffe7c41f75fbdd1
Stefan Countryman <stefan.countryman@gmail.com>
2016-03-09 04:35:56 -0500

    try fixing vagrant by using preseed for user/pass

--- vagrant.sh
+++ vagrant.sh
@@ -4,5 +4,8 @@ mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
     'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
     -O /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-chmod -R go-rwsx /home/vagrant/.ssh
+chown 700 /home/vagrant/.ssh
+chmod 600 /home/vagrant/.ssh/authorized_keys
+ls -l /home/vagrant/.ssh
+ls -l /home/vagrant/.ssh/authorized_keys
+cat /home/vagrant/.ssh/authorized_keys/*
