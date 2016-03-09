6e10f31acad39efa605378474e6dd2145d8e4220
Stefan Countryman <stefan.countryman@gmail.com>
2016-03-09 04:58:01 -0500

    barebones, tryna debug vagrant

--- vagrant.sh
+++ vagrant.sh
@@ -4,8 +4,7 @@ mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
     'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
     -O /home/vagrant/.ssh/authorized_keys
-chown 700 /home/vagrant/.ssh
-chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+chmod -R go-rwsx /home/vagrant/.ssh
 ls -l /home/vagrant/.ssh
-ls -l /home/vagrant/.ssh/authorized_keys
-cat /home/vagrant/.ssh/authorized_keys/*
+cat /home/vagrant/.ssh/authorized_keys
