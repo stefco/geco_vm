a8c4c76d310f2656df1bad371bd2938c6c5b64d5
Stefan Countryman <stefan.countryman@gmail.com>
2016-03-11 16:49:53 -0500

    scripts for bento ripping updated

--- copy-bento-paths.sh
+++ copy-bento-paths.sh
@@ -15,13 +15,14 @@ fi
 
 path_to_bento="$1"
 
+# make necessary directories
+mkdir -p floppy scripts/common scripts/ubuntu http
+
 while read line; do
-    cp -v -R "$path_to_bento/$line" .
+    cp -v -R "$path_to_bento/$line" "./$line"
 done <<__FILE_LIST__
 ubuntu-12.04-amd64.json
-http/
-VBoxGuestAdditions_*.iso
-.vbox_version
+http
 floppy/dummy_metadata.json
 scripts/common/metadata.sh
 scripts/ubuntu/update.sh
@@ -34,8 +35,8 @@ scripts/ubuntu/cleanup.sh
 scripts/common/minimize.sh
 __FILE_LIST__
 
-mv -v "ubuntu-12.04-amd64.json" "geco-vm.json"
-
 printf "\nThings you still have to do:\n\n"
 
 cat bento-notes
+
+echo
---
+++ rm-bento-paths.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# delete things that were just copied from bento
+
+set -o nounset
+set -o errexit
+set -o noclobber
+
+while read line; do
+    rm -v -r -f "$line"
+done <<__FILE_LIST__
+ubuntu-12.04-amd64.json
+http
+floppy
+scripts/common
+scripts/ubuntu
+__FILE_LIST__
