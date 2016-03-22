4207be8fd437a90e568eaf6304e476a9a5d837ae
Stefan Countryman <stefan.countryman@gmail.com>
2016-03-22 12:02:46 -0400

    add cleanup, fix guest additions, break gsi+julia
    using the bento cleanup.sh script does seem to fix the vagrant virtualbox
    guest additions issue, perhaps by removing specific linux kernels.
    
    however, it also uninstalls things, including globus commands (like gsissh),
    ligo-proxy-init, and julia. strangely, some other things remain untouched.
    it is unclear what exactly is causing this. atlas error logs will be helpful.

--- scripts/ubuntu/cleanup.sh
+++ scripts/ubuntu/cleanup.sh
@@ -1,11 +1,28 @@
 #!/bin/sh -eux
 
-# Delete all Linux headers
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE ALL LINUX HEADERS
+*
+*
+***********************************************************
+__MSG__
 dpkg --list \
   | awk '{ print $2 }' \
   | grep 'linux-headers' \
   | xargs apt-get -y purge;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* REMOVE SPECIFIC LINUX KERNELS
+*
+*
+***********************************************************
+__MSG__
 # Remove specific Linux kernels, such as linux-image-3.11.0-15-generic but
 # keeps the current kernel and does not touch the virtual packages,
 # e.g. 'linux-image-generic', etc.
@@ -15,28 +32,101 @@ dpkg --list \
     | grep -v `uname -r` \
     | xargs apt-get -y purge;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE LINUX SOURCE
+*
+*
+***********************************************************
+__MSG__
 # Delete Linux source
 dpkg --list \
     | awk '{ print $2 }' \
     | grep linux-source \
     | xargs apt-get -y purge;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE DEVELOPMENT PACKAGES
+*
+*
+***********************************************************
+__MSG__
 # Delete development packages
 dpkg --list \
     | awk '{ print $2 }' \
     | grep -- '-dev$' \
     | xargs apt-get -y purge;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE X11 LIBRARIES
+*
+*
+***********************************************************
+__MSG__
 # Delete X11 libraries
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE OBSOLETE NETWORKING
+*
+*
+***********************************************************
+__MSG__
 # Delete obsolete networking
 apt-get -y purge ppp pppconfig pppoeconf;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE ODDITIES
+*
+*
+***********************************************************
+__MSG__
 # Delete oddities
 apt-get -y purge popularity-contest;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* AUTOREMOVE PACKAGES
+*
+*
+***********************************************************
+__MSG__
 apt-get -y autoremove;
+
+cat <<__MSG__
+***********************************************************
+*
+*
+* CLEAN
+*
+*
+***********************************************************
+__MSG__
 apt-get -y clean;
 
+cat <<__MSG__
+***********************************************************
+*
+*
+* DELETE GUEST ADDITIONS ISO
+*
+*
+***********************************************************
+__MSG__
 rm -f VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?;
