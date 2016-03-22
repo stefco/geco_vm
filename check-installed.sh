e38a932c2f8bc6ca5d242aed21782f8d0a864664
Stefan Countryman <stefan.countryman@gmail.com>
2016-03-22 18:01:28 -0400

    add gsissh+ligo-proxy-init back in

--- check-installed.sh
+++ check-installed.sh
@@ -8,8 +8,8 @@ cat <<__MSG__
 ************************************************************
 __MSG__
 command -v lalapps_tconvert >/dev/null 2>&1 || { echo >&2 "I require lalapps_tconvert but it's not installed.  Aborting."; exit 1; }
-# command -v ligo-proxy-init >/dev/null 2>&1 || { echo >&2 "I require ligo-proxy-init but it's not installed.  Aborting."; exit 1; }
+command -v ligo-proxy-init >/dev/null 2>&1 || { echo >&2 "I require ligo-proxy-init but it's not installed.  Aborting."; exit 1; }
 command -v gsissh >/dev/null 2>&1 || { echo >&2 "I require gsissh but it's not installed.  Aborting."; exit 1; }
 command -v kinit >/dev/null 2>&1 || { echo >&2 "I require kinit but it's not installed.  Aborting."; exit 1; }
 command -v ipython >/dev/null 2>&1 || { echo >&2 "I require ipython but it's not installed.  Aborting."; exit 1; }
-# command -v julia >/dev/null 2>&1 || { echo >&2 "I require julia but it's not installed.  Aborting."; exit 1; }
+command -v julia >/dev/null 2>&1 || { echo >&2 "I require julia but it's not installed.  Aborting."; exit 1; }
--- provision.sh
+++ provision.sh
@@ -129,15 +129,17 @@ cat <<__MSG__
 *
 ***********************************************************
 __MSG__
-wget http://www.globus.org/ftppub/gt6/installers/repo/globus-toolkit-repo_latest_all.deb
-dpkg -i globus-toolkit-repo_latest_all.deb
+# wget http://www.globus.org/ftppub/gt6/installers/repo/globus-toolkit-repo_latest_all.deb
+# dpkg -i globus-toolkit-repo_latest_all.deb
+apt-get -y -qq install globus-toolkit-repo
 apt-get -y -qq update
 apt-get -y -qq -o Dpkg::Options::="--force-confdef" -o \
                   Dpkg::Options::="--force-confold" install \
                   globus-gridftp globus-gram5 globus-gsi \
                   globus-data-management-server globus-data-management-client \
                   globus-resource-management-client
-# rm globus-toolkit-repo_latest_all.deb*
+apt-get -y -qq install vdt-gsi-openssh-client
+rm globus-toolkit-repo_latest_all.deb*
 
 cat <<__MSG__
 ***********************************************************
@@ -150,6 +152,7 @@ cat <<__MSG__
 __MSG__
 wget -O- http://www.lsc-group.phys.uwm.edu/lscdatagrid/doc/ldg-client.sh | bash 
 ldg-version
+apt-get -y -qq install ligo-proxy-utils
 apt-get -y -qq update
 apt-get -y -qq dist-upgrade
 
