ca17a17d6507a92deeb8c8334932f7b5f636e127
Stefan Countryman <stefan.countryman@gmail.com>
2016-04-10 13:19:19 -0400

    add desktop install script (run after update)

---
+++ scripts/geco/desktop.sh
@@ -0,0 +1,2 @@
+sudo apt-get install ubuntu-desktop
+gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', 'gnome-terminal.desktop', 'firefox.desktop', 'libreoffice-writer.desktop', 'libreoffice-calc.desktop', 'libreoffice-impress.desktop', 'ubuntu-software-center.desktop', 'gnome-control-center.desktop']"
