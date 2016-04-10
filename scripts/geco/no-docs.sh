ea9b4fbf990ffa511c2675ebe1e363051dea3b82
Stefan Countryman <stefan.countryman@gmail.com>
2016-04-09 22:04:10 -0400

    try to keep man pages

--- scripts/geco/no-docs.sh
+++ scripts/geco/no-docs.sh
@@ -12,8 +12,8 @@ cat <<__EOF__ > /etc/dpkg/dpkg.cfg.d/01_nodoc
 path-exclude /usr/share/doc/*
 # we need to keep copyright files for legal reasons
 path-include /usr/share/doc/*/copyright
-path-exclude /usr/share/groff/*
-path-exclude /usr/share/info/*
+# path-exclude /usr/share/groff/*
+# path-exclude /usr/share/info/*
 # lintian stuff is small, but really unnecessary
 path-exclude /usr/share/lintian/*
 path-exclude /usr/share/linda/*
@@ -23,5 +23,5 @@ __EOF__
 
 find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true
 find /usr/share/doc -empty|xargs rmdir || true
-rm -rf /usr/share/groff/* /usr/share/info/*
+# rm -rf /usr/share/groff/* /usr/share/info/*
 rm -rf /usr/share/lintian/* /usr/share/linda/*
