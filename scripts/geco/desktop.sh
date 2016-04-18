85f65fecc1f65d505a5ddaee5bd1d6a91ea01369
Stefan Countryman <stefan.countryman@gmail.com>
2016-04-18 13:14:27 -0400

    gui building, added 256mb vram

--- scripts/geco/desktop.sh
+++ scripts/geco/desktop.sh
@@ -1,2 +1,2 @@
-sudo apt-get install ubuntu-desktop
-gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', 'gnome-terminal.desktop', 'firefox.desktop', 'libreoffice-writer.desktop', 'libreoffice-calc.desktop', 'libreoffice-impress.desktop', 'ubuntu-software-center.desktop', 'gnome-control-center.desktop']"
+sudo apt-get -y -qq install ubuntu-desktop
+# gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', 'gnome-terminal.desktop', 'firefox.desktop', 'libreoffice-writer.desktop', 'libreoffice-calc.desktop', 'libreoffice-impress.desktop', 'ubuntu-software-center.desktop', 'gnome-control-center.desktop']"
