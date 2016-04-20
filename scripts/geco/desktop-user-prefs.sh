e19642d677046860abd124fd2a8000d78c26bf6d
Stefan Countryman <stefan.countryman@gmail.com>
2016-04-20 17:26:46 -0400

    add dbus-launch --exit-with-session to make prefs update

--- scripts/geco/desktop-user-prefs.sh
+++ scripts/geco/desktop-user-prefs.sh
@@ -1,10 +1,10 @@
 # settings that probably need to be run by the user
 
 # make sure enough time has passed since startup for the desktop to start
-sleep 30
+sleep 10
 
 # disable automatic screen lock
-gsettings set org.gnome.desktop.screensaver lock-enabled false
+dbus-launch --exit-with-session gsettings set org.gnome.desktop.screensaver lock-enabled false
 
 # set the favorites on the desktop
-gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', '/home/vagrant/.medm_launcher/medm-lho.desktop', '/home/vagrant/.medm_launcher/medm-llo.desktop', 'gnome-terminal.desktop', 'firefox.desktop', 'libreoffice-writer.desktop', 'libreoffice-calc.desktop', 'libreoffice-impress.desktop']"
+dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', '/home/vagrant/.medm_launcher/medm-lho.desktop', '/home/vagrant/.medm_launcher/medm-llo.desktop', 'gnome-terminal.desktop', 'firefox.desktop']"
