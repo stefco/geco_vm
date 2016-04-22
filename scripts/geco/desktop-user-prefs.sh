7d1a7835e2f6530cebe4b0b4bb480410fa45301e
Stefan Countryman <stefan.countryman@gmail.com>
2016-04-22 16:33:37 -0400

    don't forget to add auth.desktop to launcher

--- scripts/geco/desktop-user-prefs.sh
+++ scripts/geco/desktop-user-prefs.sh
@@ -7,4 +7,4 @@ sleep 10
 dbus-launch --exit-with-session gsettings set org.gnome.desktop.screensaver lock-enabled false
 
 # set the favorites on the desktop
-dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', '/home/vagrant/.medm_launcher/medm-lho.desktop', '/home/vagrant/.medm_launcher/medm-llo.desktop', 'gnome-terminal.desktop', 'firefox.desktop']"
+dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', '/home/vagrant/.medm_launcher/auth.desktop', '/home/vagrant/.medm_launcher/medm-lho.desktop', '/home/vagrant/.medm_launcher/medm-llo.desktop', 'gnome-terminal.desktop', 'firefox.desktop']"
