06fbb62b16d5494807e0bf7df86cb8a293ca2404
Stefan Countryman <stefan.countryman@gmail.com>
2016-04-23 17:03:12 -0400

    copy entirety of home-directory

--- scripts/geco/move-files.sh
+++ scripts/geco/move-files.sh
@@ -1,16 +1,26 @@
 # when installing with packer, move files from /tmp
+
+set -o noclobber
+
+# put EPICS files where they need to be for installation
 mkdir -p /opt
 mv /tmp/epics /opt
 
-# add in extra files, like .bashrc and scripts in ~/bin
-if [ -e /tmp/.bashrc ]; then
-    cat /tmp/.bashrc >> "$HOME"/.bashrc
+# for .bashrc, just concatenate it onto the default .bashrc
+if [ -e /tmp/HOME/.bashrc ]; then
+    cat "${HOME}"/.bashrc /tmp/HOME/.bashrc > /tmp/HOME/.bashrc
 fi
-if [ -e /tmp/bin ]; then
-    if ! [ -e "$HOME"/bin ]; then
-        mkdir "$HOME"/bin
-    fi
-    mv --no-clobber /tmp/bin/* "$HOME"/bin
-    chmod +x -R "$HOME"/bin
-    echo export PATH=\"/vagrant:$HOME/bin:\$PATH\" >> ~/.bashrc
+
+# copy the rest of the stuff in the home directory recursively
+if [ -e /tmp/HOME ]; then
+    # this sneakily copies the contents of /tmp/HOME into ${HOME}
+    cp -R /tmp/HOME/. "${HOME}"
 fi
+
+# make a directory for executables if it doesn't exist already
+if ! [ -e "$HOME"/bin ]; then
+    mkdir "$HOME"/bin
+fi
+
+chmod +x -R "$HOME"/bin
+echo export PATH=\"/vagrant:${HOME}/bin:\$PATH\" >> ~/.bashrc
