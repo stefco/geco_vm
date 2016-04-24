2aa52f8e36ea4e4f7e40f3161c3d77c025c69af2
Stefan Countryman <stefco@users.noreply.github.com>
2016-04-24 15:57:43 -0400

    change default terminal font

--- scripts/geco/desktop-user-prefs.sh
+++ scripts/geco/desktop-user-prefs.sh
@@ -1,4 +1,4 @@
-# settings that probably need to be run by the user
+# desktop settings
 
 # make sure enough time has passed since startup for the desktop to start
 sleep 10
@@ -8,3 +8,7 @@ dbus-launch --exit-with-session gsettings set org.gnome.desktop.screensaver lock
 
 # set the favorites on the desktop
 dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "['nautilus-home.desktop', '/home/vagrant/.medm_launcher/auth.desktop', '/home/vagrant/.medm_launcher/medm-lho.desktop', '/home/vagrant/.medm_launcher/medm-llo.desktop', 'gnome-terminal.desktop', 'firefox.desktop']"
+
+# set default terminal font to Deja Vu Sans, 9 point font
+dbus-launch --exit-with-session gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=boolean false
+dbus-launch --exit-with-session gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Deja Vu Sans Mono 9"
