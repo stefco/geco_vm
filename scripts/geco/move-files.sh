45848f50e212c182db8a70181da4871b850aade2
vagrant <vagrant@vagrant.vm>
2016-05-25 18:13:01 +0000

    add .vimrc

--- scripts/geco/move-files.sh
+++ scripts/geco/move-files.sh
@@ -4,24 +4,25 @@
 mkdir -p /opt
 mv /tmp/epics /opt
 
-# for .bashrc, just concatenate it onto the default .bashrc
+# for .bashrc, concatenate the default OS .bashrc to the start
 if [ -e /tmp/HOME/.bashrc ]; then
     cat "${HOME}"/.bashrc /tmp/HOME/.bashrc > /tmp/bashrc-tmp \
         && mv /tmp/bashrc-tmp /tmp/HOME/.bashrc
 fi
 
-# copy the rest of the stuff in the home directory recursively
+# now copy everything in the home directory recursively
 if [ -e /tmp/HOME ]; then
     # this sneakily copies the contents of /tmp/HOME into ${HOME}
     cp -R /tmp/HOME/. "${HOME}"
 fi
 
-# make a directory for executables if it doesn't exist already
+# make a directory for executables if it doesn't exist already (though it was
+# likely copied over in the last step)
 if ! [ -e "$HOME"/bin ]; then
     mkdir "$HOME"/bin
 fi
 
-# make bin executable and add to PATH
+# make ~/bin executable and add it to $PATH on login
 chmod +x -R "$HOME"/bin
 echo export PATH=\"/vagrant:${HOME}/bin:\$PATH\" >> ~/.bashrc
 
